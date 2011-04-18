d6e2323798a0377d1fc9ce8a0b48b55aa5cf32ad
Aki Salmi <aki.salmi@iki.fi>
2011-04-18 08:18:23 +0300

    adding 'pending' step definitions

--- tests/cucumber/features/step_definitios/game_steps.rb
+++ tests/cucumber/features/step_definitios/game_steps.rb
@@ -20,42 +20,14 @@ When /^I roll '(\d+)'$/ do |roll|
   @game.roll(roll)
 end
 
-Then /^the score (\d+)st frame of the Score Card contains '(.*)'$/ do |frame_number, frame_content|
-  rolls_for_frame(frame_number.to_i).should include frame_content
-end
-
-def rolls_for_frame(frame_number)
-  score_card = @output_spy.messages().detect{|str| str.include?('Score Card')}.split("\n")
-  score_card_score_row = score_card[3]
-  score_card_rolls = score_card[2]
-  score_card_frame_numbers = score_card[1]
-  return score_card_rolls.split("|")[frame_number]  
-end
-
 When /^I roll '(\d+) \/ (\d+)'$/ do |first, second|
   pending
   @game.roll(first)
   @game.roll(second)
 end
 
-Then /^I the score of the (\d+)st frame is (\d+)$/ do |frame_number, score|
-  score_for_frame(frame_number.to_i).should == score.to_s
-end
-
-def score_for_frame(frame_number)
-  score_card = @output_spy.messages().detect{|str| str.include?('Score Card')}.split("\n")
-  score_card_score_row = score_card[3]
-  score_card_rolls = score_card[2]
-  score_card_frame_numbers = score_card[1]
-  return score_card_rolls.split("|")[frame_number]  
-end
-
-Then /^the score should be (\d+)$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-When /^I roll (\d+) pins$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I roll (\d+) pins$/ do |roll|
+  When("I roll '#{roll}'")
 end
 
 When /^I roll following pins:$/ do |table|
@@ -66,3 +38,40 @@ end
 When /^I strike only strikes$/ do
   pending # express the regexp above with the code you wish you had
 end
+
+Then /^the score (\d+)st frame of the Score Card contains '(.*)'$/ do |frame_number, frame_content|
+  rolls_for_frame(frame_number.to_i).should include frame_content
+end
+
+def rolls_for_frame(frame_number)
+  parse_score_card()[:rolls].split("|")[frame_number]  
+end
+
+Then /^I the score of the (\d+)st frame is (\d+)$/ do |frame_number, score|
+  score_for_frame(frame_number.to_i).should == score.to_s
+end
+
+def score_for_frame(frame_number)
+  return parse_score_card(){:score}.split("|")[frame_number]  
+end
+
+def parse_score_card()
+  score_card = {}
+  score_card_as_string = @output_spy.messages().detect{|str| str.include?('Score Card')}.split("\n")
+  score_card[:score] = score_card_as_string[3]
+  score_card[:rolls] = score_card_as_string[2]
+  return score_card
+end
+
+module ScoreCardParser
+  
+  def [](frame_number)
+    return 
+  end
+  
+end
+
+Then /^the score should be (\d+)$/ do |expected_score|
+  @game.score.should == expected_score.to_i
+end
+
