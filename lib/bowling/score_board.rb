04db4c6dbfba1b4f1eaf3fefc2070a12d2bd9a33
Aki Salmi <aki.salmi@iki.fi>
2011-04-29 16:58:29 +0300

    support for strikes, 10th frame missing

--- lib/bowling/score_board.rb
+++ lib/bowling/score_board.rb
@@ -3,11 +3,9 @@ module Bowling
   
   class ScoreBoard
     
-    attr_reader :current_frame
-    
     def initialize()
       @current_frame = 1
-      @scored_frame_index = 0
+      @scored_frame = 0
       @frames = []
       10.times do
         @frames << Frame.new()
@@ -15,25 +13,53 @@ module Bowling
     end
     
     def roll( pins )
-      frame( current_frame() ).roll(pins)
-      if can_calculate_score?( @scored_frame_index + 1)
-        calculate_score()
-      end
-      if frame_ready?( current_frame() ) 
-        @current_frame += 1         
-      end
+      frame( @current_frame ).roll(pins)
+      
+      calculate_score()
+      increment_frame_index_if_necessary()
     end 
     
     def score()
-      frame(@scored_frame_index).score()
+      frame(@scored_frame).score()
+    end
+    
+    def frame( frame_index )
+      return @frames[ frame_index - 1 ]
     end
     
     private
-    def can_calculate_score?( frame_index )
-      if spare?( frame( frame_index ) ) 
-        return frame( frame_index + 1 ).rolls.length > 0
+    def increment_frame_index_if_necessary()
+      if frame_ready?( @current_frame ) 
+        @current_frame += 1         
       end
-      return frame_index <= @current_frame && frame_ready?( @current_frame)
+    end
+    
+    def calculate_score()
+      (@scored_frame ... @current_frame).each do
+        if can_calculate_score?( @scored_frame + 1)
+          calculate_frame_score()
+        end
+      end
+    end
+    
+    def can_calculate_score?( frame_index )
+      
+      if strike?( frame( frame_index ) )
+        if strike?( frame( frame_index + 1 ) )
+          return frame( frame_index + 1 + 1 ).rolls.length == 1
+        end
+        return frame( frame_index + 1 ).rolls.length == 2
+      
+      elsif spare?( frame( frame_index ) ) 
+        return frame( frame_index + 1 ).rolls.length == 1
+      
+      else
+        return frame_index <= @current_frame && frame_ready?( @current_frame)        
+      end
+    end
+    
+    def strike?( frame )
+      return frame.rolls[0] == 10
     end
     
     def spare?( frame )
@@ -41,24 +67,25 @@ module Bowling
     end
     
     def frame_ready?( frame_number )
-      return frame( current_frame() ).rolls().length == 2
+      return frame( @current_frame ).rolls().length == 2 || frame( @current_frame ).rolls[0] == 10
     end
     
-    public
-    def frame( frame_index )
-      return @frames[ frame_index - 1 ]
-    end
-    
-    def calculate_score() 
-      _score = 10
-      _score_of_previous_frame = frame(@current_frame - 1).score
-      if spare?( frame( @scored_frame_index + 1 ) )
-        _score = _score_of_previous_frame + 10 + frame( @scored_frame_index + 1 + 1).rolls[0]
+    def calculate_frame_score() 
+      _score = 0
+      _score_of_previous_frame = frame(@scored_frame).score
+      if strike?( frame( @scored_frame + 1 ) )
+        if strike?( frame( @scored_frame + 1 + 1 ) ) 
+          _score = _score_of_previous_frame + 10 + 10 + frame( @scored_frame + 1 + 1 + 1 ).rolls().inject(0,:+)
+        else 
+          _score = _score_of_previous_frame + 10 + frame( @scored_frame + 1 + 1).rolls().inject(0,:+)
+        end
+      elsif spare?( frame( @scored_frame + 1 ) )
+        _score = _score_of_previous_frame + 10 + frame( @scored_frame + 1 + 1).rolls().inject(0,:+)
       else
-        _score = _score_of_previous_frame + frame(@current_frame).rolls().inject(:+)
+        _score = _score_of_previous_frame + frame(@current_frame).rolls().inject(0,:+)
       end
-      frame(@scored_frame_index + 1).score = _score
-      @scored_frame_index += 1
+      frame(@scored_frame + 1).score = _score
+      @scored_frame += 1
     end
     
   end
--- tests/rspec/bowling/score_board_spec.rb
+++ tests/rspec/bowling/score_board_spec.rb
@@ -2,12 +2,19 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'spec_helper'
 require 'bowling'
 
+module CurrentFrame
+  def current_frame()
+    @current_frame
+  end
+end
+
 module Bowling
   describe ScoreBoard do
 
     let( :score_board ) { ScoreBoard.new() }
     
     it "should start from frame number 1" do
+      score_board.extend CurrentFrame
       score_board.current_frame().should == 1    
     end
 
@@ -18,6 +25,7 @@ module Bowling
     end
 
     it "should know that frame number 2 is after two throws on first frame" do
+      score_board.extend CurrentFrame
       2.times{ score_board.roll(4) }
       score_board.current_frame().should == 2
     end
@@ -38,6 +46,41 @@ module Bowling
       score_board.roll(10)
       score_board.frame(1).score().should == 20
       score_board.score().should == 20
+    end
+    
+    it "should know that a strike is only throw on a frame" do
+      score_board.extend CurrentFrame
+      score_board.roll(10)
+      score_board.current_frame().should == 2
+    end
+    
+    it "should calculate score for strike only after two rolls after the strike" do
+      score_board.roll(10)
+      score_board.frame(1).score().should == 0
+      score_board.roll(1)
+      score_board.frame(1).score().should == 0
+      score_board.roll(1)
+      score_board.frame(1).score().should == 12
+    end
+
+    it "should calculate three consecutive strikes" do
+      3.times{ score_board.roll(10) }
+      score_board.frame(1).score().should == 30 
+      score_board.score().should == 30
+    end
+
+    it "should calculate strikes, spares and misses" do
+      2.times{ score_board.roll(10) }
+      # 3rd frame
+      score_board.roll(9)
+      score_board.score().should == 29
+      score_board.roll(1)
+      score_board.score().should == 49
+      # 4th frame
+      score_board.roll(1)
+      score_board.score().should == 60
+      score_board.roll(8)
+      score_board.score().should == 69
     end
 
   end
